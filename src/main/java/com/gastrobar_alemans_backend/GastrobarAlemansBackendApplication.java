8298c761e6d640b28fb14d56a2d5dfcf7e7124ba
JosueGarcia04 <josueadrian042006@gmail.com>
2025-04-14 18:38:10 -0600

    first

---
+++ src/main/java/com/gastrobar_alemans_backend/GastrobarAlemansBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gastrobar_alemans_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastrobarAlemansBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastrobarAlemansBackendApplication.class, args);
+    }
+
+}
